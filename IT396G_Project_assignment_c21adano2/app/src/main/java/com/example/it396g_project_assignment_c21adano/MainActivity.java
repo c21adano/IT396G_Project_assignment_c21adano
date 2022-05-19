e2b5d2df500934f5a1eee9977073860721e4488b
c21adano <102599332+c21adano@users.noreply.github.com>
2022-05-19 01:30:08 +0100

    Add files via upload

---
+++ IT396G_Project_assignment_c21adano2/app/src/main/java/com/example/it396g_project_assignment_c21adano/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.it396g_project_assignment_c21adano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ IT396G_Project_assignment_c21adano2/app/src/main/java/com/example/it396g_project_assignment_c21adano/test.java
@@ -0,0 +1,4 @@
+package com.example.it396g_project_assignment_c21adano;
+
+public class test {
+}
